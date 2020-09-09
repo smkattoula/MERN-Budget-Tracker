d9ab02544154064421f5e58a0be39fb26a068cbd
Shaker Kattoula <smkattoula@gmail.com>
2020-09-09 16:37:22 -0700

    prepare post build for deploymentt to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+const path = require("path");
 const cors = require("cors");
 
 const transactions = require("./routes/api/transactions");
@@ -30,6 +31,16 @@ mongoose
 // Use Routes
 app.use("/api/transactions", transactions);
 app.use("/api/users", users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
